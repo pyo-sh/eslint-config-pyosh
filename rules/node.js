31a9b255c487ac0b0027370d9a58c1fcea5e16a4
pyo-sh <pygosky@gmail.com>
2023-10-17 16:04:49 +0900

    feat: node 실행에 eslint로 방해될 수 있는 요소들 off

---
+++ rules/node.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "env": {
+    "node": true
+  },
+
+  "rules": {
+    // https://eslint.org/docs/latest/rules/callback-return
+    "callback-return": "off",
+
+    // https://eslint.org/docs/rules/global-require
+    "global-require": "off",
+
+    // https://eslint.org/docs/latest/rules/handle-callback-err
+    "handle-callback-err": "off",
+
+    // https://eslint.org/docs/rules/no-buffer-constructor
+    "no-buffer-constructor": "off",
+
+    // https://eslint.org/docs/latest/rules/no-mixed-requires
+    "no-mixed-requires": ["off", false],
+
+    // https://eslint.org/docs/latest/rules/no-new-require
+    "no-new-require": "off",
+
+    // https://eslint.org/docs/rules/no-path-concat
+    "no-path-concat": "off",
+
+    // https://eslint.org/docs/latest/rules/no-process-env
+    "no-process-env": "off",
+
+    // https://eslint.org/docs/latest/rules/no-process-exit
+    "no-process-exit": "off",
+
+    // https://eslint.org/docs/latest/rules/no-restricted-modules
+    "no-restricted-modules": "off",
+
+    // https://eslint.org/docs/latest/rules/no-sync
+    "no-sync": "off",
+  }
+};
